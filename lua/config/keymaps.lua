dd4b0c11c1f96c2c27ac645849ec02d1dd05bf12
MSA611 <shoaib9542186823@gmail.com>
2025-10-14 15:01:17 +0530

    added nvim-tree and jk to enter into exit mode in insert mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,6 +5,8 @@ end, { desc = "Format file or selection" })
 
 vim.keymap.set("n", "<Esc>", ":nohlsearch<CR><Esc>", { noremap = true, silent = true }) -- to disable highlighting in esc
 
+vim.keymap.set("i", "jk", "<Esc>")
+
 -- save file
 vim.keymap.set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
 
