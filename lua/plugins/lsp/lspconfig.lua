2db8cbe24d85d53136e7d74b810b27a19e656a5a
msa-developer <shoaib9542186823@gmail.com>
2025-10-24 20:37:22 +0530

    added proper working of lsp configuration with blink.cmp

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -1,1 +1,16 @@
-return{}
+return {
+  "neovim/nvim-lspconfig",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    { "saghen/blink.cmp" },
+    { "antosha417/nvim-lsp-file-operations", config = true },
+    { "folke/lazydev.nvim",                  opts = {} },
+  },
+  config = function()
+    local blink = require("blink.cmp")
+    local capabilities = blink.get_lsp_capabilities()
+    vim.lsp.config("*", {
+      capabilities = capabilities,
+    })
+  end,
+}
