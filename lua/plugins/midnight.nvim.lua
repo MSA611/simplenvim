adf75b1b829abddd7248d2894394ee7d6bd50379
msa-developer <shoaib9542186823@gmail.com>
2025-11-01 07:41:44 +0530

    changed the appearence and behaviour of blink.cmp according to the midnight nvim theme

--- lua/plugins/midnight.nvim.lua
+++ lua/plugins/midnight.nvim.lua
@@ -6,6 +6,7 @@ return {
 		config = function()
 			vim.cmd("colorscheme midnight")
 			vim.api.nvim_set_hl(0, "Search", { bg = "#FFA500", fg = "Black" })
+			vim.api.nvim_set_hl(0, "BlinkCmpGhostText", { fg = "#7f8ba7", bg = "NONE", italic = true })
 		end,
 	},
 }
